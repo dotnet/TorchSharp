ea312e0b204a119a1b647aac41dcdd595c8dd99d
Miguel de Icaza <miguel@gnome.org>
2018-10-12 15:20:25 -0400

    Skeleton

---
+++ PytorchSharp/Tester/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class MainClass {
+	public static void Main (string [] args)
+	{
+		Console.WriteLine ("Hello World!");
+	}
+}
---
+++ PytorchSharp/TypeGeneration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PytorchSharp {
+
+    public class ByteStorage : IDisposable {
+        IntPtr handle;
+        
+        [DllImport ("caffe2")]
+        extern static IntPtr THByteStorage_new ();
+        
+        public ByteStorage ()
+        {
+            handle = THByteStorage_new ();
+        }
+        
+        [DllImport ("caffe2")]
+        extern static IntPtr THByteStorage_new_with_size (IntPtr size);
+        
+        public ByteStorage (long size)
+        {
+            handle = THByteStorage_new_with_size ((IntPtr) size);
+        }
+        
+        ~ByteStorage ()
+        {
+            Dispose (false);
+        }
+        
+        public void Dispose ()
+        {
+            Dispose (true);
+            GC.SuppressFinalize (this);
+        }
+        
+        public void Dispose (bool disposing)
+        {
+            if (disposing){
+                
+                THByteStorage_free (handle);
+                handle = IntPtr.Zero;
+            }
+        }
+        
+        public byte this [long index] {
+            get {
+                return  default;
+            }
+            set {
+            }
+        }
+    }
