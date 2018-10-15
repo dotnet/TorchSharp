537365102c30bb28bf83382a879938753e53177b
Miguel de Icaza <miguel@gnome.org>
2018-10-15 16:49:50 -0400

    More work, rename to TorchSharp

---
+++ PytorchSharp/THRandom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TorchSharp {
+	/// <summary>
+	/// Random class
+	/// </summary>
+    	/// <remarks>
+	/// Behind the scenes this is the THGenerator API and THRandom combined into
+    	/// one as THRandom are just convenience methods on top of THGenerator.
+    	/// </remarks>
+	public class THRandom : IDisposable {
+		internal IntPtr handle;
+
+		[DllImport ("caffe2")]
+		extern static void THGenerator_free (IntPtr handle);
+
+		[DllImport ("caffe2")]
+		extern static IntPtr THGenerator_new ();
+
+		public Random ()
+		{
+			handle = THGenerator_new ();
+		}
+
+		[DllImport ("caffe2")]
+		extern static int THGeneratorState_isValid (IntPtr handle);
+
+		public bool IsValid => THGeneratorState_isValid (handle) != 0;
+
+		protected virtual void Dispose (bool disposing)
+		{
+			if (handle != IntPtr.Zero) {
+				THGenerator_free (handle);
+				handle = IntPtr.Zero;
+			}
+		}
+
+		~Random ()
+		{
+			Dispose (false);
+		}
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TorchSharp;
+
+class MainClass {
+	public static void Main (string [] args)
+	{
+		var x = new FloatTensor (10);
+		x.Random (new THRandom (), 10);
+
+	}
+}
