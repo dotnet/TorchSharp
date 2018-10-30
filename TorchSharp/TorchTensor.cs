81f0d8029f72c68be3a74f480f1c04aa2c449e67
Matteo Interlandi <mainterl@microsoft.com>
2018-10-29 20:42:33 -0700

    First version of TorchFloatTensor extending DenseTensor

---
+++ TorchSharp/TorchTensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics.Tensors;
+
+namespace TorchSharp
+{
+    public class TorchFloatTensor : DenseTensor<float>
+    {
+        FloatTensor inner;
+
+        public static unsafe TorchFloatTensor Create(int length)
+        {
+            var inner = new FloatTensor(length);
+            var mem = new NativeMemory<float>((void*)inner.Data, length);
+            return new TorchFloatTensor(mem.Memory, new int[] { length }, inner);
+        }
+
+        public TorchFloatTensor(Memory<float> memory, ReadOnlySpan<int> dimensions, FloatTensor inner) : base(memory, dimensions)
+        {
+            this.inner = inner;
+        }
+
+        public void Fill(int value)
+        {
+            inner.Fill(value);
+        }
+    }
+}
