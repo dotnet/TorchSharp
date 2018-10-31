e05326eba20197ed42c6bc4669f9bd665249574a
Matteo Interlandi <mainterl@microsoft.com>
2018-10-31 09:40:43 -0700

    Simplification of the constructor. Test for accessing records.

--- Test/FloatTorchTensorUnitTest.cs
+++ Test/FloatTorchTensorUnitTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
+using System.Linq;
 using System.Numerics.Tensors;
 using TorchSharp;
 
@@ -11,7 +13,7 @@ namespace TorchSharp.Tests
         [TestMethod]
         public void TestCreation0D()
         {
-            Assert.ThrowsException<ArgumentOutOfRangeException>(FloatTorchTensor.Create);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => FloatTorchTensor.Create());
         }
 
         [TestMethod]
@@ -107,6 +109,65 @@ namespace TorchSharp.Tests
                 for (int j = 0; j < x.Dimensions[1]; j++)
                 {
                     Assert.AreEqual(x[i, j], 30.0f);
+                }
+            }
+        }
+
+        internal class FloatTorchTensorMockup : DenseTensor<float>
+        {
+            private readonly FloatTensor inner;
+
+            public static unsafe FloatTorchTensorMockup Create(params int[] sizes)
+            {
+                var memLen = 0;
+                var shape = sizes;
+
+                if (sizes.Length == 0)
+                {
+                    shape = new int[] { 0 };
+                }
+                else
+                {
+                    memLen = sizes.Aggregate((a, b) => a * b);
+                }
+
+                var inner = new FloatTensor(sizes.Select(x => (long)x).ToArray());
+                var mem = new NativeMemory<float>((void*)inner.Data, memLen);
+
+                return new FloatTorchTensorMockup(mem.Memory, shape, inner);
+            }
+
+            public FloatTorchTensorMockup(Memory<float> memory, ReadOnlySpan<int> dimensions, FloatTensor inner) : base(memory, dimensions)
+            {
+                this.inner = inner;
+            }
+
+            public float Get(long x0, long x1)
+            {
+                return inner[x0, x1];
+            }
+        }
+
+        [TestMethod]
+        public void TestFillEquivalance2D()
+        {
+            var x = FloatTorchTensorMockup.Create(10, 10);
+            var rand = new Random();
+            
+            for (int i = 0; i < x.Dimensions[0]; i++)
+            {
+                for (int j = 0; j < x.Dimensions[1]; j++)
+                {
+                    var tmp = (float)rand.NextDouble();
+                    x[i, j] = tmp;
+                }
+            }
+
+            for (int i = 0; i < x.Dimensions[0]; i++)
+            {
+                for (int j = 0; j < x.Dimensions[1]; j++)
+                {
+                    Assert.AreEqual(x[i, j], x.Get(i, j));
                 }
             }
         }
--- TorchSharp/TorchTensor.cs
+++ TorchSharp/TorchTensor.cs
@@ -6,47 +6,37 @@ namespace TorchSharp
 {
     public sealed class FloatTorchTensor : DenseTensor<float>
     {
-        private readonly FloatTensor inner;
-
-        /// <summary>
-        ///   Utility method to create an empty TorchTensor.
-        ///   This is currently failing because SNT does not support zero-size tensors
-        /// </summary>
-        public static unsafe FloatTorchTensor Create()
-        {
-            var inner = new FloatTensor();
-            return new FloatTorchTensor(new Memory<float>(), new int[1] { 0 }, inner);
-        }
+        internal readonly FloatTensor inner;
 
         /// <summary>
         ///   Utility method to create a TorchTensor.
+        ///   This is currently failing if the input parameter is empty because SNT 
+        ///   does not support zero-size tensors
         /// </summary>
         /// <param name="sizes">The desired sizes for the dimensions of the tensor.</param>
         public static unsafe FloatTorchTensor Create(params int[] sizes)
         {
+            var memLen = 0;
+            var shape = sizes;
+
             if (sizes.Length == 0)
             {
-                return Create();
+                shape = new int[] { 0 };
+            }
+            else
+            {
+                memLen = sizes.Aggregate((a, b) => a * b);
             }
 
             var inner = new FloatTensor(sizes.Select(x => (long)x).ToArray());
-            var mem = new NativeMemory<float>((void*)inner.Data, sizes.Aggregate((a, b) => a * b));
-            return new FloatTorchTensor(mem.Memory, sizes, inner);
+            var mem = new NativeMemory<float>((void*)inner.Data, memLen);
+
+            return new FloatTorchTensor(mem.Memory, shape, inner);
         }
 
         public FloatTorchTensor(Memory<float> memory, ReadOnlySpan<int> dimensions, FloatTensor inner) : base(memory, dimensions)
         {
             this.inner = inner;
-        }
-
-        /// <summary>
-        /// Creates a shallow copy of this tensor, with new backing storage.
-        /// </summary>
-        /// <returns>A shallow copy of this tensor.</returns>
-        public override Tensor<float> Clone()
-        {
-            var innerClone = new FloatTensor(inner.Shape);
-            return new FloatTorchTensor(Buffer.ToArray(), Dimensions, inner);
         }
     }
 }
