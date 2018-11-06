141ba135aabd0246d08936782c4073c2dd5dc879
Matteo Interlandi <mainterl@microsoft.com>
2018-11-05 20:19:07 -0800

    Added SNT

---
+++ SNT/NativeMemoryManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Buffers;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System;
+
+namespace Torch.SNT
+{
+    internal class NativeMemory<T> : MemoryManager<T>
+    {
+        private int refCount = 0;
+        private IntPtr memory;
+        private int length;
+
+        public NativeMemory(IntPtr memory, int length)
+        {
+            this.memory = memory;
+            this.length = length;
+        }
+
+        public unsafe NativeMemory(void* memory, int length)
+        {
+            this.memory = (IntPtr)memory;
+            this.length = length;
+        }
+
+        /// <summary>
+        /// The Tensor memory lifecycle is managed by Torch. Disposing the memory
+        /// from here will through a "double free or corruption" error.
+        /// <summary>
+        ~NativeMemory()
+        {
+            Dispose(false);
+        }
+
+        public static NativeMemory<T> Allocate(int length)
+        {
+            // typically this would call into a native method appropriate for the platform
+            // or the constructors above would be used to wrap the native pointer
+            IntPtr memory = Marshal.AllocHGlobal(Marshal.SizeOf<T>() * length);
+            return new NativeMemory<T>(memory, length);
+        }
+
+        public bool IsDisposed { get; private set; } = false;
+
+        public unsafe override Span<T> GetSpan() => new Span<T>((void*)memory, length);
+
+        protected bool IsRetained => refCount > 0;
+
+        public override MemoryHandle Pin(int elementIndex = 0)
+        {
+            unsafe
+            {
+                Retain();
+                if ((uint)elementIndex > length) throw new ArgumentOutOfRangeException(nameof(elementIndex));
+                void* pointer = Unsafe.Add<T>((void*)memory, elementIndex);
+                return new MemoryHandle(pointer, default, this);
+            }
+        }
+
+        public bool Release()
+        {
+            int newRefCount = Interlocked.Decrement(ref refCount);
+
+            if (newRefCount < 0)
+            {
+                throw new InvalidOperationException("Unmatched Release/Retain");
+            }
+
+            return newRefCount != 0;
+        }
+
+        public void Retain()
+        {
+            if (IsDisposed)
+            {
+                throw new ObjectDisposedException(nameof(NativeMemory<T>));
+            }
+
+            Interlocked.Increment(ref refCount);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // typically this would call into a native method appropriate for the platform
+                Marshal.FreeHGlobal(memory);
+                memory = IntPtr.Zero;
+            }
+
+            IsDisposed = true;
+        }
+
+        protected override bool TryGetArray(out ArraySegment<T> arraySegment)
+        {
+            // cannot expose managed array
+            arraySegment = default;
+            return false;
+        }
+
+        public override void Unpin()
+        {
+            Release();
+        }
+    }
+}
---
+++ SNT/TypeGeneration.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Numerics.Tensors;
+using TorchSharp;
+
+namespace Torch.SNT
+{
+    public sealed class FloatTorchTensor : DenseTensor<float>
+    {
+        internal readonly object inner;
+
+        public FloatTensor TorchSharpTensor =>  inner as FloatTensor;
+
+        public FloatTorchTensor(Memory<float> memory, ReadOnlySpan<int> dimensions, FloatTensor inner) : base(memory, dimensions)
+        {
+            this.inner = inner;
+        }
+
+        /// <summary>
+        ///   Utility method to create a TorchTensor.
+        ///   This is currently failing if the input parameter is empty because SNT 
+        ///   does not support zero-size tensors.
+        /// </summary>
+        /// <param name="sizes">The desired sizes for the dimensions of the tensor.</param>
+        public static unsafe FloatTorchTensor Create(params int[] sizes)
+        {
+            var totLength = Utils.GetTotalLength(sizes);
+            var shape = sizes;
+
+            if (sizes.Length == 0)
+            {
+                shape = new int[] { 0 };
+            }
+
+            var inner = new FloatTensor(sizes.Select(x => (long)x).ToArray());
+            var mem = new NativeMemory<float>(inner.Data, totLength);
+
+            return new FloatTorchTensor(mem.Memory, shape, inner);
+        }
+
+        /// <summary>
+        /// Creates a shallow copy of this tensor, with new backing storage.
+        /// </summary>
+        /// <returns>A shallow copy of this tensor.</returns>
+        public unsafe override Tensor<float> Clone()
+        {
+            var typedInner = inner as FloatTensor;
+            var innerClone = typedInner.Clone();
+            var mem = new NativeMemory<float>(innerClone.Data, Buffer.Length);
+
+            return new FloatTorchTensor(mem.Memory, Dimensions, innerClone);
+        }
+
+        /// <summary>
+        /// Creates a new Tensor of a different type with the specified dimensions and the same layout as this tensor with elements initialized to their default value.
+        /// </summary>
+        /// <typeparam name="TResult">Type contained in the returned Tensor.</typeparam>
+        /// <param name="dimensions">An span of integers that represent the size of each dimension of the DenseTensor to create.</param>
+        /// <returns>A new tensor with the same layout as this tensor but different type and dimensions.</returns>
+        public override unsafe Tensor<TResult> CloneEmpty<TResult>(ReadOnlySpan<int> dimensions)
+        {
+            var typedInner = inner as FloatTensor;
+
+            switch (true)
+            {
+                case bool _ when typeof(TResult) == typeof(float):
+                    var innerClone = new FloatTensor(typedInner.Shape);
+                    innerClone.Fill(default);
+                    var mem = new NativeMemory<float>(innerClone.Data, Buffer.Length);
+
+                    return new FloatTorchTensor(mem.Memory, Dimensions, innerClone) as Tensor<TResult>;
+                default: throw new NotImplementedException("Only cloning floats is currently implemented.");
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the current tensor to new dimensions, using the same backing storage.
+        /// </summary>
+        /// <param name="dimensions">An span of integers that represent the size of each dimension of the DenseTensor to create.</param>
+        /// <returns>A new tensor that reinterprets backing Buffer of this tensor with different dimensions.</returns>
+        public unsafe override Tensor<float> Reshape(ReadOnlySpan<int> dimensions)
+        {
+            if (dimensions.Length == 0)
+            {
+                throw new ArgumentException("Dimensions must contain elements.", nameof(dimensions));
+            }
+
+            var newSize = Utils.GetTotalLength(dimensions);
+
+            if (newSize != Length)
+            {
+                throw new ArgumentException($"Cannot reshape array due to mismatch in lengths, currently {Length} would become {newSize}.", nameof(dimensions));
+            }
+
+            var typedInner = inner as FloatTensor;
+            FloatTensor reshapedTensor;
+            NativeMemory<float> mem;
+
+            switch (dimensions.Length)
+            {
+                case 1:
+                    reshapedTensor = typedInner.NewWithStorage1d(IntPtr.Zero, dimensions[0], 1);
+                    mem = new NativeMemory<float>(reshapedTensor.Data, Buffer.Length);
+                    break;
+                case 2:
+                    reshapedTensor = typedInner.NewWithStorage2d(IntPtr.Zero, dimensions[0], dimensions[1], dimensions[1], 1);
+                    mem = new NativeMemory<float>(reshapedTensor.Data, Buffer.Length);
+                    break;
+                default: throw new ArgumentException($"Cannot reshape tensor with more than 4 dimensions");
+            }
+
+            return new FloatTorchTensor(mem.Memory, dimensions, reshapedTensor);
+        }
+    }
+}
