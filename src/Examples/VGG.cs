deb8d95746b53a2c944960cb9b3787211169abc0
Sumit Kumar <kusu@microsoft.com>
2019-07-25 11:33:31 -0700

    initial commit

---
+++ src/Examples/VGG.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TorchSharp.NN;
+using TorchSharp.Tensor;
+
+namespace Examples
+{
+    /// <summary>
+    ///     
+    /// </summary>
+    class VGG : TorchSharp.NN.Module
+    {
+        Sequential sequential;
+        private readonly static Dictionary<string, List<object>> cfg;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="VGGName"></param>
+        public VGG(string VGGName) : base()
+        {
+            // super(VGG, self).__init__()  dont really need it, i think
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private override TorchTensor Forward(TorchTensor input)
+        {
+
+        }
+
+        private Sequential MakeSequential(List<string> cfg)
+        {
+            List<Module> layers = new List<Module>();
+            int in_channels = 3;
+
+            foreach (object x in cfg)
+            {
+                if (x.ToString() == "M")
+                    layers.Add(Module.MaxPool2D(new long[2], new long[1]));
+                else
+                {
+                    layers.Add(Module.Conv2D(in_channels, (long)x, 3, padding: 1));
+                    //layers.Add(Module.Bat);
+
+                }
+
+            }
+        }
+
+    }
+}
