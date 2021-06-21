df18a3b3c615c1ca6696876a01e1955a1b91abc9
Don Syme <donsyme@fastmail.com>
2021-06-21 19:22:42 +0100

    fix versions

--- src/TorchSharp/Torch.cs
+++ src/TorchSharp/Torch.cs
@@ -14,7 +14,7 @@ namespace TorchSharp
 
     public static class Torch
     {
-        const string libtorchPackageVersion = "1.9.0.1";
+        const string libtorchPackageVersion = "1.9.0.3";
         const string cudaVersion = "11.1";
 
         [DllImport("LibTorchSharp")]
