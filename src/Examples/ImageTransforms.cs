08a81a178552fdf93d0d8cff8f4afe01bd932c70
Niklas Gustafsson <niklasg@microsoft.com>
2021-09-01 13:12:22 -0700

    Fixed copyright header formatting in one file.

--- src/Examples/ImageTransforms.cs
+++ src/Examples/ImageTransforms.cs
@@ -1,4 +1,5 @@
-// Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.using System;
+// Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
