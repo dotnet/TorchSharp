103396943e096a5b9d640f7277b59d2f57c8f365
Niklas Gustafsson <niklas.gustafsson@microsoft.com>
2021-10-14 16:35:18 -0700

    Added muli-index indexing to TensorAccessor.

--- src/FSharp.Examples/TextClassification.fs
+++ src/FSharp.Examples/TextClassification.fs
@@ -55,9 +55,9 @@ type TextClassificationModel(vocabSize, embedDim, nClasses, device:torch.Device)
     do
         let initrange = 0.5
 
-        init.uniform(embedding.Weight, -initrange, initrange) |> ignore
-        init.uniform(fc.Weight, -initrange, initrange) |> ignore
-        init.zeros(fc.Bias) |> ignore
+        init.uniform_(embedding.Weight, -initrange, initrange) |> ignore
+        init.uniform_(fc.Weight, -initrange, initrange) |> ignore
+        init.zeros_(fc.Bias) |> ignore
 
         this.RegisterComponents()
 
