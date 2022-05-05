6fff2619f19229b6b6a1bad1be041a54550e9d8d
Niklas Gustafsson <niklasg@microsoft.com>
2022-05-04 17:58:46 -0700

    Unit tests

--- test/TorchSharpTest/TestTorchVision.cs
+++ test/TorchSharpTest/TestTorchVision.cs
@@ -14,7 +14,7 @@ namespace TorchSharp
         [Fact]
         public void TestResNet18()
         {
-            var model = resnet18();
+            using var model = resnet18();
             var sd = model.state_dict();
             Assert.Equal(122, sd.Count);
         }
@@ -22,7 +22,7 @@ namespace TorchSharp
         [Fact]
         public void TestResNet34()
         {
-            var model = resnet34();
+            using var model = resnet34();
             var sd = model.state_dict();
             Assert.Equal(218, sd.Count);
         }
@@ -30,7 +30,7 @@ namespace TorchSharp
         [Fact]
         public void TestResNet50()
         {
-            var model = resnet50();
+            using var model = resnet50();
             var sd = model.state_dict();
             Assert.Equal(320, sd.Count);
         }
@@ -38,7 +38,7 @@ namespace TorchSharp
         [Fact]
         public void TestResNet101()
         {
-            var model = resnet101();
+            using var model = resnet101();
             var sd = model.state_dict();
             Assert.Equal(626, sd.Count);
         }
@@ -46,7 +46,7 @@ namespace TorchSharp
         [Fact]
         public void TestResNet152()
         {
-            var model = resnet152();
+            using var model = resnet152();
             var sd = model.state_dict();
             Assert.Equal(932, sd.Count);
         }
@@ -54,7 +54,7 @@ namespace TorchSharp
         [Fact]
         public void TestAlexNet()
         {
-            var model = alexnet();
+            using var model = alexnet();
             var sd = model.state_dict();
             Assert.Equal(16, sd.Count);
         }
@@ -63,12 +63,12 @@ namespace TorchSharp
         public void TestVGG11()
         {
             {
-                var model = vgg11();
+                using var model = vgg11();
                 var sd = model.state_dict();
                 Assert.Equal(22, sd.Count);
             }
             {
-                var model = vgg11_bn();
+                using var model = vgg11_bn();
                 var sd = model.state_dict();
                 Assert.Equal(62, sd.Count);
             }
@@ -78,12 +78,12 @@ namespace TorchSharp
         public void TestVGG13()
         {
             {
-                var model = vgg13();
+                using var model = vgg13();
                 var sd = model.state_dict();
                 Assert.Equal(26, sd.Count);
             }
             {
-                var model = vgg13_bn();
+                using var model = vgg13_bn();
                 var sd = model.state_dict();
                 Assert.Equal(76, sd.Count);
             }
@@ -93,12 +93,12 @@ namespace TorchSharp
         public void TestVGG16()
         {
             {
-                var model = vgg16();
+                using var model = vgg16();
                 var sd = model.state_dict();
                 Assert.Equal(32, sd.Count);
             }
             {
-                var model = vgg16_bn();
+                using var model = vgg16_bn();
                 var sd = model.state_dict();
                 Assert.Equal(97, sd.Count);
             }
@@ -108,12 +108,12 @@ namespace TorchSharp
         public void TestVGG19()
         {
             {
-                var model = vgg19();
+                using var model = vgg19();
                 var sd = model.state_dict();
                 Assert.Equal(38, sd.Count);
             }
             {
-                var model = vgg19_bn();
+                using var model = vgg19_bn();
                 var sd = model.state_dict();
                 Assert.Equal(118, sd.Count);
             }
