9e90219c94ff04264df648f31cf5269cd5f501fa
Alawode Oluwandabira <olalaw@ttu.ee>
2022-06-25 21:21:49 +0200

    Refactor ImagerSharp

--- src/Examples.Utils/ImagerSharp.cs
+++ src/Examples.Utils/ImagerSharp.cs
@@ -16,15 +16,6 @@ namespace TorchSharp.Examples.Utils
         /// </summary>
         public sealed class ImagerSharp : io.Imager
         {
-            private Tensor ToTensor<TPixel>(byte[] bytes) where TPixel : unmanaged, IPixel<TPixel>
-            {
-                var image = Image.Load<TPixel>(bytes);
-                var channels = Unsafe.SizeOf<TPixel>();
-                byte[] imageBytes = new byte[image.Height * image.Width * channels];
-                image.CopyPixelDataTo(imageBytes);
-                return tensor(imageBytes, new long[] { image.Height, image.Width, channels}).permute(2, 0, 1);
-            }
-
             private Tensor ToTensor<TPixel>(Stream stream) where TPixel : unmanaged, IPixel<TPixel>
             {
                 var image = Image.Load<TPixel>(stream);
@@ -32,21 +23,6 @@ namespace TorchSharp.Examples.Utils
                 byte[] imageBytes = new byte[image.Height * image.Width * channels];
                 image.CopyPixelDataTo(imageBytes);
                 return tensor(imageBytes, new long[] { image.Height, image.Width, channels }).permute(2, 0, 1);
-            }
-
-            private byte[] FromTensor<TPixel>(Tensor t, ImageFormat format) where TPixel : unmanaged, IPixel<TPixel>
-            {
-                var shape = t.shape;
-                var tt = t.reshape(new long[] { shape[0] * shape[1] * shape[2] });
-                var image = Image.LoadPixelData<TPixel>(tt.data<byte>().ToArray(), (int)shape[1], (int)shape[0]);
-                IImageEncoder encoder = format switch {
-                   ImageFormat.Png => new PngEncoder(),
-                   ImageFormat.Jpeg => new JpegEncoder(),
-                   _ => throw new ArgumentException("Cannot encode to Unknown format"),
-                };
-                var stream = new MemoryStream();
-                encoder.Encode(image, stream);
-                return stream.ToArray();
             }
 
             private void FromTensor<TPixel>(Tensor t, ImageFormat format, Stream stream) where TPixel : unmanaged, IPixel<TPixel>
@@ -64,25 +40,8 @@ namespace TorchSharp.Examples.Utils
 
             public override Tensor DecodeImage(byte[] bytes, io.ImageReadMode mode = io.ImageReadMode.UNCHANGED)
             {
-                switch (mode) {
-
-                case io.ImageReadMode.UNCHANGED:
-                    var format = Image.DetectFormat(bytes);
-                    if (format is PngFormat) {
-                        return ToTensor<Rgba32>(bytes);
-                    } else {
-                        return ToTensor<Rgb24>(bytes);
-                    }
-                case io.ImageReadMode.RGB_ALPHA:
-                    return ToTensor<Rgba32>(bytes);
-                case io.ImageReadMode.RGB:
-                    return ToTensor<Rgb24>(bytes);
-                case io.ImageReadMode.GRAY_ALPHA:
-                    return ToTensor<La16>(bytes);
-                case io.ImageReadMode.GRAY:
-                    return ToTensor<L8>(bytes);
-                default: throw new NotImplementedException();
-                }
+                using (var stream = new MemoryStream(bytes))
+                    return DecodeImage(stream, mode);
             }
 
             public override Tensor DecodeImage(Stream stream, io.ImageReadMode mode = io.ImageReadMode.UNCHANGED)
@@ -109,14 +68,10 @@ namespace TorchSharp.Examples.Utils
 
             public override byte[] EncodeImage(Tensor image, ImageFormat format)
             {
-                Tensor permuted = image.permute(1, 2, 0);
-                return image.shape[0] switch {
-                    1 => FromTensor<L8>(permuted, format),
-                    2 => FromTensor<La16>(permuted, format),
-                    3 => FromTensor<Rgb24>(permuted, format),
-                    4 => FromTensor<Rgba32>(permuted, format),
-                    _ => throw new ArgumentException("image tensor must have a colour channel of 1,2,3 or 4")
-                };
+                using (var stream = new MemoryStream()) {
+                    EncodeImage(image, format, stream);
+                    return stream.ToArray();
+                }
             }
 
             public override void EncodeImage(Tensor image, ImageFormat format, Stream stream)
--- src/FSharp.Examples/ImageTransforms.fs
+++ src/FSharp.Examples/ImageTransforms.fs
@@ -12,19 +12,20 @@ open type TorchSharp.torchvision.transforms
 let run (argv : string[]) =
     DefaultImager <- new Utils.ImagerSharp()
 
-    let img = read_image(argv.[0])
+    let filename : string = argv.[0]
+    let img = read_image(filename)
     let shape = img.shape
 
     printfn "Image has %d color channels and dimensions %dx%d" (shape.[0]) (shape.[1]) (shape.[2])
 
-    let filename = Path.GetFileNameWithoutExtension(argv.[0])
-    let dir = Path.GetDirectoryName(argv.[0])
+    let fname = Path.GetFileNameWithoutExtension(filename)
+    let dir = Path.GetDirectoryName(filename)
 
-    let transform = torchvision.transforms.Compose(HorizontalFlip(), Rotate(50f), CenterCrop(256))
+    let transform = torchvision.transforms.Compose(HorizontalFlip(), Rotate(50f), Resize(256, 256))
 
     let transformed = transform.forward(img)
 
-    let tfile = Path.Combine(dir, filename + "-transformed.png")
+    let tfile = Path.Combine(dir, fname + "-transformed.png")
 
     write_image(transformed, tfile , torchvision.ImageFormat.Png)
 
@@ -33,9 +34,9 @@ let run (argv : string[]) =
     let functional_transformed =
         functional.hflip(img)
         |> fun t -> functional.rotate(t, 50f)
-        |> fun t -> functional.center_crop(t, 256)
+        |> fun t -> functional.resize(t, 256, 256)
         
-    let ftfile = Path.Combine(dir, filename + "-functransformed.png")
+    let ftfile = Path.Combine(dir, fname + "-func-transformed.png")
 
     write_image(functional_transformed, ftfile, torchvision.ImageFormat.Png)
 
