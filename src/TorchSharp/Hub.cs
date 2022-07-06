a3788aefe05c880bb3d6c7fb861821819c3995b2
Katsuya Iida <katsuya.iida@gmail.com>
2022-07-06 14:55:50 +0900

    Web client has been deprecated and should to replaced with HttpClient

--- src/TorchSharp/Hub.cs
+++ src/TorchSharp/Hub.cs
@@ -1,9 +1,10 @@
 // Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.
 using System;
 using System.IO;
-using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TorchSharp
 {
@@ -21,16 +22,45 @@ namespace TorchSharp
             /// <exception cref="InvalidDataException">SHA256 hash doesn't match</exception>
             public static void download_url_to_file(string url, string dst, string hash_prefix = null, bool progress = true)
             {
-                using (var webClient = new WebClient()) {
-                    if (progress) {
-                        // TODO: Implement progress.
-                        // Console.WriteLine("download_url_to_file(progress: true) is not supported.");
+                try {
+                    Task.Run(async () => {
+                        await download_url_to_file_async(url, dst, hash_prefix, progress);
+                    }).Wait();
+                } catch (AggregateException ex) {
+                    throw ex.InnerException;
+                }
+            }
+
+            /// <summary>
+            /// Download the url to a file 
+            /// </summary>
+            /// <param name="url">The URL to download</param>
+            /// <param name="dst">The file path to download the URL into</param>
+            /// <param name="hash_prefix">If non null, the SHA256 hash of downloaded content must match this prefix</param>
+            /// <param name="progress">Display the progress bar</param>
+            /// <exception cref="InvalidDataException">SHA256 hash doesn't match</exception>
+            public static async Task download_url_to_file_async(string url, string dst, string hash_prefix = null, bool progress = true)
+            {
+                using (var httpClient = new HttpClient()) {
+                    using (var response = await httpClient.GetAsync(url)) {
+                        if (!response.IsSuccessStatusCode) {
+                            throw new InvalidDataException();
+                        }
+                        using (var writer = File.OpenWrite(dst)) {
+                            if (progress) {
+                                // TODO: Implement progress.
+                                // Console.WriteLine("download_url_to_file(progress: true) is not supported.");
+                                await response.Content.CopyToAsync(writer);
+                            } else {
+                                await response.Content.CopyToAsync(writer);
+                            }
+                        }
                     }
-                    webClient.DownloadFile(url, dst);
-                    string fileHash = GetFileChecksum(dst);
-                    if (hash_prefix != null && !fileHash.StartsWith(hash_prefix)) {
-                        throw new InvalidDataException($"invalid hash value (expected \"{hash_prefix}\", got \"{fileHash}\"");
-                    }
+                }
+
+                string fileHash = GetFileChecksum(dst);
+                if (hash_prefix != null && !fileHash.StartsWith(hash_prefix)) {
+                    throw new InvalidDataException($"invalid hash value (expected \"{hash_prefix}\", got \"{fileHash}\"");
                 }
             }
 
