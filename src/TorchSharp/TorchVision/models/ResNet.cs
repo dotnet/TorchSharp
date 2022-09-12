2d909c19de3bf6c8fe24daca31c94607b1334412
Niklas Gustafsson <niklasg@microsoft.com>
2022-09-12 09:17:36 -0700

    Fixed bug in ResNet implementation.

--- src/TorchSharp/TorchVision/models/ResNet.cs
+++ src/TorchSharp/TorchVision/models/ResNet.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using TorchSharp.Modules;
 using TorchSharp.torchvision.Modules;
 using static TorchSharp.torch;
 using static TorchSharp.torch.nn;
@@ -220,10 +221,10 @@ namespace TorchSharp.torchvision
             private readonly Module bn1;
             private readonly Module relu1;
 
-            private readonly TorchSharp.Modules.ModuleList layer1 = new TorchSharp.Modules.ModuleList();
-            private readonly TorchSharp.Modules.ModuleList layer2 = new TorchSharp.Modules.ModuleList();
-            private readonly TorchSharp.Modules.ModuleList layer3 = new TorchSharp.Modules.ModuleList();
-            private readonly TorchSharp.Modules.ModuleList layer4 = new TorchSharp.Modules.ModuleList();
+            private readonly Sequential layer1 = Sequential();
+            private readonly Sequential layer2 = Sequential();
+            private readonly Sequential layer3 = Sequential();
+            private readonly Sequential layer4 = Sequential();
 
             private readonly Module avgpool;
             private readonly Module maxpool;
@@ -356,7 +357,7 @@ namespace TorchSharp.torchvision
                     this.to(device);
             }
 
-            private void MakeLayer(TorchSharp.Modules.ModuleList modules, Func<int, int, int, Module> block, int expansion, int planes, int num_blocks, int stride)
+            private void MakeLayer(Sequential modules, Func<int, int, int, Module> block, int expansion, int planes, int num_blocks, int stride)
             {
                 var strides = new List<int>();
                 strides.Add(stride);
@@ -375,10 +376,10 @@ namespace TorchSharp.torchvision
 
                     var x = maxpool.forward(relu1.forward(bn1.forward(conv1.forward(input))));
 
-                    foreach (var m in layer1) x = m.forward(x);
-                    foreach (var m in layer2) x = m.forward(x);
-                    foreach (var m in layer3) x = m.forward(x);
-                    foreach (var m in layer4) x = m.forward(x);
+                    x = layer1.forward(x);
+                    x = layer2.forward(x);
+                    x = layer3.forward(x);
+                    x = layer4.forward(x);
 
                     var res = fc.forward(flatten.forward(avgpool.forward(x)));
                     scope.MoveToOuter(res);
