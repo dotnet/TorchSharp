73e3a6101a8778943b46e802a573870b1a7aa95f
Yangyu Huang <yanghuan@microsoft.com>
2022-09-13 16:12:41 +0800

    remove comments to avoid xml check

--- src/TorchSharp/TorchVision/Ops.cs
+++ src/TorchSharp/TorchVision/Ops.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.
 
 using static TorchSharp.torch;
+using System.Range;
+using System.Index;
 using System.Collections.Generic;
 
 namespace TorchSharp.torchvision
@@ -9,8 +11,7 @@ namespace TorchSharp.torchvision
     {
         public static Tensor sigmoid_focal_loss(Tensor inputs, Tensor targets, float alpha = 0.25f, float gamma = 2.0f, nn.Reduction reduction = nn.Reduction.None)
         {
-            using (var _ = torch.NewDisposeScope())
-            {
+            using (var _ = torch.NewDisposeScope()) {
 
                 var p = torch.sigmoid(inputs);
                 var ce_loss = nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction: reduction);
@@ -18,18 +19,14 @@ namespace TorchSharp.torchvision
 
                 var loss = ce_loss * (1 - p_t).pow(gamma);
 
-                if (alpha >= 0)
-                {
+                if (alpha >= 0) {
                     var alpha_t = alpha * targets + (1 - alpha) * (1 - targets);
                     loss = alpha_t * loss;
                 }
 
-                if (reduction == nn.Reduction.Mean)
-                {
+                if (reduction == nn.Reduction.Mean) {
                     loss = loss.mean();
-                }
-                else if (reduction == nn.Reduction.Sum)
-                {
+                } else if (reduction == nn.Reduction.Sum) {
                     loss = loss.sum();
                 }
 
@@ -37,17 +34,9 @@ namespace TorchSharp.torchvision
             }
         }
 
-        /// <summary>
-        /// Performs non-maximum suppression (NMS) on the boxes according to their intersection-over-union (IoU). NMS iteratively removes lower scoring boxes which have an IoU greater than iou_threshold with another(higher scoring) box.
-        /// </summary>
-        /// <param name="boxes">boxes (Tensor[N, 4]) to perform NMS on. They are expected to be in (x1, y1, x2, y2) format with 0 <= x1 < x2 and 0 <= y1 < y2.</param>
-        /// <param name="scores">scores (Tensor[N]) for each one of the boxes.</param>
-        /// <param name="iou_threshold">discards all overlapping boxes with IoU > iou_threshold.</param>
-        /// <returns>the indices (Tensor) of the elements that have been kept by NMS, sorted in decreasing order of scores.</returns>
         public static Tensor nme(Tensor boxes, Tensor scores, double iou_threshold = 0.5)
         {
-            using (var nmsScope = torch.NewDisposeScope())
-            {
+            using (var nmsScope = torch.NewDisposeScope()) {
                 var x1 = boxes[.., 0];
                 var y1 = boxes[.., 1];
                 var x2 = boxes[.., 2];
@@ -57,17 +46,13 @@ namespace TorchSharp.torchvision
                 var order = _order[..];
 
                 var keep = new List<long>();
-                while (order.numel() > 0)
-                {
+                while (order.numel() > 0) {
                     long i;
-                    if (order.numel() == 1)
-                    {
+                    if (order.numel() == 1) {
                         i = order.item<long>();
                         keep.Add(i);
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         i = order[0].item<long>();
                         keep.Add(i);
                     }
@@ -80,8 +65,7 @@ namespace TorchSharp.torchvision
 
                     var iou = inter / (areas[i] + areas[order[1..]] - inter);
                     var idx = (iou <= iou_threshold).nonzero().squeeze();
-                    if (idx.numel() == 0)
-                    {
+                    if (idx.numel() == 0) {
                         break;
                     }
 
