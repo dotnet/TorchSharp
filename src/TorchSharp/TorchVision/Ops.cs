17ec845c2a22812ebf61bf3647e4baf28314a66f
Yangyu Huang <yanghuan@microsoft.com>
2022-09-15 15:40:24 +0800

    change index method

--- src/TorchSharp/TorchVision/Ops.cs
+++ src/TorchSharp/TorchVision/Ops.cs
@@ -1,9 +1,10 @@
 // Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.
 
 using static TorchSharp.torch;
-using System.Range;
-using System.Index;
+using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace TorchSharp.torchvision
 {
@@ -36,40 +37,47 @@ namespace TorchSharp.torchvision
 
         public static Tensor nme(Tensor boxes, Tensor scores, double iou_threshold = 0.5)
         {
-            using (var nmsScope = torch.NewDisposeScope()) {
-                var x1 = boxes[.., 0];
-                var y1 = boxes[.., 1];
-                var x2 = boxes[.., 2];
-                var y2 = boxes[.., 3];
+            using (var nmsScope = torch.NewDisposeScope())
+            {
+                var x1 = boxes.select(1, 0);
+                var y1 = boxes.select(1, 1);
+                var x2 = boxes.select(1, 2);
+                var y2 = boxes.select(1, 3);
                 var areas = (x2 - x1) * (y2 - y1);
-                var (_, _order) = scores.sort(0, descending: true);
-                var order = _order[..];
+                var (_, order) = scores.sort(0, descending: true);
 
                 var keep = new List<long>();
-                while (order.numel() > 0) {
+                while (order.numel() > 0)
+                {
                     long i;
-                    if (order.numel() == 1) {
-                        i = order.item<long>();
+                    if (order.numel() == 1)
+                    {
+                        i = order.cpu().item<long>();
                         keep.Add(i);
                         break;
-                    } else {
-                        i = order[0].item<long>();
+                    }
+                    else
+                    {
+                        i = order[0].cpu().item<long>();
                         keep.Add(i);
                     }
 
-                    var xx1 = x1[order[1..]].clamp(min: x1[i]);
-                    var yy1 = y1[order[1..]].clamp(min: y1[i]);
-                    var xx2 = x2[order[1..]].clamp(max: x2[i]);
-                    var yy2 = y2[order[1..]].clamp(max: y2[i]);
+                    var indices = torch.arange(1, order.shape[0], dtype: ScalarType.Int64);
+                    order = order[indices];
+                    var xx1 = x1[order].clamp(min: x1[i]);
+                    var yy1 = y1[order].clamp(min: y1[i]);
+                    var xx2 = x2[order].clamp(max: x2[i]);
+                    var yy2 = y2[order].clamp(max: y2[i]);
                     var inter = (xx2 - xx1).clamp(min: 0) * (yy2 - yy1).clamp(min: 0);
 
-                    var iou = inter / (areas[i] + areas[order[1..]] - inter);
+                    var iou = inter / (areas[i] + areas[order] - inter);
                     var idx = (iou <= iou_threshold).nonzero().squeeze();
-                    if (idx.numel() == 0) {
+                    if (idx.numel() == 0)
+                    {
                         break;
                     }
 
-                    order = order[idx + 1];
+                    order = order[idx];
                 }
 
                 var ids = torch.from_array(keep.ToArray()).to_type(ScalarType.Int64).to(device: boxes.device);
