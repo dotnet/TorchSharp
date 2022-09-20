1870c820c6e630ab375b3846c9aa6c18ee042f7e
Katsuya Iida <katsuya.iida@gmail.com>
2022-09-21 08:33:26 +0900

    `Conv2dNormActivation` doesn't add layers when `norm_layer` or `activation_layer` is null.

--- src/TorchSharp/TorchVision/Ops/Misc.cs
+++ src/TorchSharp/TorchVision/Ops/Misc.cs
@@ -21,6 +21,65 @@ namespace TorchSharp
     {
         public static partial class ops
         {
+            private static nn.Module ConvNormActivation(
+                long in_channels,
+                long out_channels,
+                long kernel_size = 3,
+                long stride = 1,
+                long? padding = null,
+                long groups = 1,
+                Func<long, nn.Module>? norm_layer = null,
+                Func<bool, nn.Module>? activation_layer = null,
+                long dilation = 1,
+                bool inplace = true,
+                bool? bias = null,
+                int rank = 2)
+            {
+                if (padding == null) {
+                    padding = (kernel_size - 1) / 2 * dilation;
+                }
+
+                if (bias == null) {
+                    bias = norm_layer == null;
+                }
+
+                var layers = new List<nn.Module>();
+                if (rank == 2) {
+                    layers.Add(
+                        nn.Conv2d(
+                            in_channels,
+                            out_channels,
+                            kernelSize: kernel_size,
+                            stride: stride,
+                            padding: padding.Value,
+                            dilation: dilation,
+                            groups: groups,
+                            bias: bias.Value));
+                } else if (rank == 3) {
+                    layers.Add(
+                        nn.Conv3d(
+                            in_channels,
+                            out_channels,
+                            kernelSize: kernel_size,
+                            stride: stride,
+                            padding: padding.Value,
+                            dilation: dilation,
+                            groups: groups,
+                            bias: bias.Value));
+                } else {
+                    throw new ArgumentOutOfRangeException("rank must be 2 or 3.");
+                }
+
+                if (norm_layer != null) {
+                    layers.Add(norm_layer(out_channels));
+                }
+
+                if (activation_layer != null) {
+                    layers.Add(activation_layer(inplace));
+                }
+                return nn.Sequential(layers);
+            }
+
             /// <summary>
             /// Configurable block used for Convolution2d-Normalization-Activation blocks.
             /// </summary>
@@ -30,11 +89,11 @@ namespace TorchSharp
             /// <param name="stride">Stride of the convolution.</param>
             /// <param name="padding">Padding added to all four sides of the input. Default: null, in which case it will calculated as ``padding = (kernel_size - 1) // 2 * dilation``</param>
             /// <param name="groups">Number of blocked connections from input channels to output channels.</param>
-            /// <param name="norm_layer">Norm layer that will be stacked on top of the convolution layer. If ``null``, ``torch.nn.BatchNorm2d`` is used.</param>
-            /// <param name="activation_layer">Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``null``, ``torch.nn.ReLU`` is used.</param>
+            /// <param name="norm_layer">Norm layer that will be stacked on top of the convolution layer. If ``null`` this layer wont be used.</param>
+            /// <param name="activation_layer">Activation function which will be stacked on top of the normalization layer (if not null), otherwise on top of the conv layer. If ``null`` this layer wont be used.</param>
             /// <param name="dilation">Spacing between kernel elements.</param>
             /// <param name="inplace">Parameter for the activation layer, which can optionally do the operation in-place.</param>
-            /// <param name="bias">Whether to use bias in the convolution layer. By default, is false.</param>
+            /// <param name="bias">Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is null``.</param>
             public static nn.Module Conv2dNormActivation(
                 long in_channels,
                 long out_channels,
@@ -48,37 +107,19 @@ namespace TorchSharp
                 bool inplace = true,
                 bool? bias = null)
             {
-                if (padding == null) {
-                    padding = (kernel_size - 1) / 2 * dilation;
-                }
-
-                if (bias == null) {
-                    bias = false;
-                }
-
-                var layers = new List<nn.Module> {
-                    nn.Conv2d(
-                        in_channels,
-                        out_channels,
-                        kernelSize: kernel_size,
-                        stride: stride,
-                        padding: padding.Value,
-                        dilation: dilation,
-                        groups: groups,
-                        bias: bias.Value)
-                };
-                if (norm_layer == null) {
-                    layers.Add(torch.nn.BatchNorm2d(out_channels));
-                } else {
-                    layers.Add(norm_layer(out_channels));
-                }
-
-                if (activation_layer == null) {
-                    layers.Add(torch.nn.ReLU(inplace));
-                } else {
-                    layers.Add(activation_layer(inplace));
-                }
-                return nn.Sequential(layers);
+                return ConvNormActivation(
+                    in_channels,
+                    out_channels,
+                    kernel_size,
+                    stride,
+                    padding,
+                    groups,
+                    norm_layer,
+                    activation_layer,
+                    dilation,
+                    inplace,
+                    bias,
+                    rank: 2);
             }
 
             /// <summary>
@@ -90,11 +131,11 @@ namespace TorchSharp
             /// <param name="stride">Stride of the convolution.</param>
             /// <param name="padding">Padding added to all four sides of the input. Default: null, in which case it will calculated as ``padding = (kernel_size - 1) // 2 * dilation``</param>
             /// <param name="groups">Number of blocked connections from input channels to output channels.</param>
-            /// <param name="norm_layer">Norm layer that will be stacked on top of the convolution layer. If ``null``, ``torch.nn.BatchNorm3d`` is used.</param>
-            /// <param name="activation_layer">Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``null``, ``torch.nn.ReLU`` is used.</param>
+            /// <param name="norm_layer">Norm layer that will be stacked on top of the convolution layer. If ``null`` this layer wont be used.</param>
+            /// <param name="activation_layer">Activation function which will be stacked on top of the normalization layer (if not null), otherwise on top of the conv layer. If ``null`` this layer wont be used.</param>
             /// <param name="dilation">Spacing between kernel elements.</param>
             /// <param name="inplace">Parameter for the activation layer, which can optionally do the operation in-place.</param>
-            /// <param name="bias">Whether to use bias in the convolution layer. By default, is false.</param>
+            /// <param name="bias">Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is null``.</param>
             public static nn.Module Conv3dNormActivation(
                 long in_channels,
                 long out_channels,
@@ -108,37 +149,19 @@ namespace TorchSharp
                 bool inplace = true,
                 bool? bias = null)
             {
-                if (padding == null) {
-                    padding = (kernel_size - 1) / 2 * dilation;
-                }
-
-                if (bias == null) {
-                    bias = false;
-                }
-
-                var layers = new List<nn.Module> {
-                    nn.Conv3d(
-                        in_channels,
-                        out_channels,
-                        kernelSize: kernel_size,
-                        stride: stride,
-                        padding: padding.Value,
-                        dilation: dilation,
-                        groups: groups,
-                        bias: bias.Value)
-                };
-                if (norm_layer == null) {
-                    layers.Add(torch.nn.BatchNorm3d(out_channels));
-                } else {
-                    layers.Add(norm_layer(out_channels));
-                }
-
-                if (activation_layer == null) {
-                    layers.Add(torch.nn.ReLU(inplace));
-                } else {
-                    layers.Add(activation_layer(inplace));
-                }
-                return nn.Sequential(layers);
+                return ConvNormActivation(
+                    in_channels,
+                    out_channels,
+                    kernel_size,
+                    stride,
+                    padding,
+                    groups,
+                    norm_layer,
+                    activation_layer,
+                    dilation,
+                    inplace,
+                    bias,
+                    rank: 3);
             }
         }
     }
