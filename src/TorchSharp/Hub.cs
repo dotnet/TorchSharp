c9748cd8b0f6b522cebf090e16bd60708e02c5b5
Katsuya Iida <katsuya.iida@gmail.com>
2022-10-14 21:49:37 +0900

    `torch.hub.download_url_to_file()` supports progress bar

--- src/TorchSharp/Hub.cs
+++ src/TorchSharp/Hub.cs
@@ -13,18 +13,41 @@ namespace TorchSharp
     {
         public static partial class hub
         {
+            private static Func<bool, IProgressBar> createProgressBarFunc = null;
+
+            /// <summary>
+            /// Set a function to create progress bar.
+            /// </summary>
+            /// <param name="func">A progress bar function or null to set default the console progress bar</param>
+            public static void set_create_progress_bar_func(Func<bool, IProgressBar> func)
+            {
+                createProgressBarFunc = func;
+            }
+
+            internal static IProgressBar _create_progress_bar(bool hidden)
+            {
+                IProgressBar progress_bar;
+                if (createProgressBarFunc == null) {
+                    progress_bar = new ConsoleProgressBar(hidden);
+                } else {
+                    progress_bar = createProgressBarFunc(hidden);
+                }
+                return progress_bar;
+            }
+
             /// <summary>
             /// Download the url to a file 
             /// </summary>
             /// <param name="url">The URL to download</param>
             /// <param name="dst">The file path to download the URL into</param>
             /// <param name="hash_prefix">If non null, the SHA256 hash of downloaded content must match this prefix</param>
+            /// <param name="progress">whether or not to display a progress bar to stderr</param>
             /// <exception cref="InvalidDataException">SHA256 hash doesn't match</exception>
-            public static void download_url_to_file(string url, string dst, string hash_prefix = null)
+            public static void download_url_to_file(string url, string dst, string hash_prefix = null, bool progress = true)
             {
                 try {
                     Task.Run(async () => {
-                        await download_url_to_file_async(url, dst, hash_prefix);
+                        await download_url_to_file_async(url, dst, hash_prefix, progress);
                     }).Wait();
                 } catch (AggregateException ex) {
                     throw ex.InnerException;
@@ -38,14 +61,27 @@ namespace TorchSharp
             /// <param name="dst">The file path to download the URL into</param>
             /// <param name="cancellationToken">A cancellation token</param>
             /// <param name="hash_prefix">If non null, the SHA256 hash of downloaded content must match this prefix</param>
+            /// <param name="progress">whether or not to display a progress bar to stderr</param>
             /// <exception cref="InvalidDataException">SHA256 hash doesn't match</exception>
-            public static async Task download_url_to_file_async(string url, string dst, string hash_prefix = null, CancellationToken cancellationToken = default)
+            public static async Task download_url_to_file_async(string url, string dst, string hash_prefix = null, bool progress = true, CancellationToken cancellationToken = default)
             {
                 try {
-                    using (var httpClient = new HttpClient()) {
-                        using (var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken)) {
-                            response.EnsureSuccessStatusCode();
-                            using (var writer = File.OpenWrite(dst)) {
+                    using (var progress_bar = _create_progress_bar(!progress))
+                    using (var httpClient = new HttpClient())
+                    using (var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken)) {
+                        response.EnsureSuccessStatusCode();
+                        progress_bar.Maximum = response.Content.Headers.ContentLength;
+                        using (var writer = File.OpenWrite(dst)) {
+                            byte[] buffer = new byte[64 * 1024];
+                            if (progress) {
+                                var reader = await response.Content.ReadAsStreamAsync();
+                                while (true) {
+                                    int ret = await reader.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+                                    if (ret == 0) break;
+                                    await writer.WriteAsync(buffer, 0, ret, cancellationToken);
+                                    progress_bar.Value += ret;
+                                }
+                            } else {
                                 await response.Content.CopyToAsync(writer);
                             }
                         }
