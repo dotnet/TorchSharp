60d528ca7ca85dfafa93a180f52f639787dab628
Niklas Gustafsson <niklasg@microsoft.com>
2023-02-15 17:16:36 -0800

    Customize the UTF8 encoder.

--- src/TorchSharp/Utils/StringEncoder.cs
+++ src/TorchSharp/Utils/StringEncoder.cs
@@ -4,10 +4,12 @@ namespace TorchSharp.Utils
 {
     internal static class StringEncoder
     {
+        private static readonly Encoding s_utfEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false);
+
         internal static byte[] GetNullTerminatedUTF8ByteArray(string input)
         {
-            var bytes = new byte[Encoding.UTF8.GetMaxByteCount(input.Length)+1];
-            var len = Encoding.UTF8.GetBytes(input, 0, input.Length, bytes, 0);
+            var bytes = new byte[s_utfEncoding.GetMaxByteCount(input.Length)+1];
+            var len = s_utfEncoding.GetBytes(input, 0, input.Length, bytes, 0);
             bytes[len] = 0;
             return bytes;
         }
