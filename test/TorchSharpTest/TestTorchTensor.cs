9e7ce68a79b6ca6106045aa01709d8679fa1e7ea
Niklas Gustafsson <niklasg@microsoft.com>
2023-04-27 07:46:41 -0700

    Support System.Index conversion to TensorIndex.

--- test/TorchSharpTest/TestTorchTensor.cs
+++ test/TorchSharpTest/TestTorchTensor.cs
@@ -3562,8 +3562,15 @@ namespace TorchSharp
             var t2 = i[1..2, 0];
             Assert.Equal(6, t2[0].ToInt32());
 
+            t2 = i[1, ^1];
+            Assert.Equal(4, t2.item<long>());
+
             // two slice
             var t3 = i[1..2, 1..3];
+            Assert.Equal(5, t3[0, 0].ToInt32());
+            Assert.Equal(4, t3[0, 1].ToInt32());
+
+            t3 = i[1..2, ^2..];
             Assert.Equal(5, t3[0, 0].ToInt32());
             Assert.Equal(4, t3[0, 1].ToInt32());
 
