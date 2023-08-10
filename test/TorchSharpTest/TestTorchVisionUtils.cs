e47340572b494f01a0bec59edf5a3817ccd1c411
Giles Bathgate <giles.bathgate@gmail.com>
2023-08-10 21:51:25 +0100

    Add test for make_grid exception with 1D input

--- test/TorchSharpTest/TestTorchVisionUtils.cs
+++ test/TorchSharpTest/TestTorchVisionUtils.cs
@@ -14,7 +14,16 @@ namespace TorchSharp
             using (var d = torch.NewDisposeScope()) {
                 torchvision.utils.save_image(image, ms, torchvision.ImageFormat.Png, imager: imager);
                 Assert.Equal(0, d.DisposablesCount);
-            } 
+            }
+        }
+
+        [Fact]
+        public void Make_Grid_IncorrectInput()
+        {
+            Assert.Throws<System.Runtime.InteropServices.ExternalException>(() => {
+                using var image = torch.tensor(new[] { 1.0f, 0.0f });
+                using var result = torchvision.utils.make_grid(image);
+            });
         }
     }
 }
