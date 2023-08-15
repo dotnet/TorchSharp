cb8516d01a07f3df71606f788bc3860f30cda45f
Giles Bathgate <giles.bathgate@gmail.com>
2023-08-15 20:42:19 +0100

    Port AugMix from pytorch

--- src/TorchVision/AutoAugment.cs
+++ src/TorchVision/AutoAugment.cs
@@ -122,13 +122,11 @@ namespace TorchSharp
             public Tensor call(Tensor img)
             {
                 using var _ = torch.NewDisposeScope();
-                var cDim = img.Dimensions - 3;
-                var height = img.shape[cDim + 1];
-                var width = img.shape[cDim + 2];
+                var (_, height, width) = F.get_dimensions(img);
                 var op_meta = augmentation_space(num_magnitude_bins, (height, width));
                 for (int i = 0; i < num_ops; ++i) {
                     var op_index = torch.randint(0, op_meta.Count, 1).ToInt32();
-                    var op_name = op_meta.Keys.ToList()[op_index];
+                    var op_name = op_meta.Keys.ElementAt(op_index);
                     var (magnitudes, signed) = op_meta[op_name];
                     var magnitude = magnitudes.Dimensions > 0 ? magnitudes[this.magnitude].ToDouble() : 0.0;
 
@@ -167,6 +165,108 @@ namespace TorchSharp
             private readonly IList<float>? fill;
         }
 
+        internal class AugMix :AutoAugmentBase, ITransform
+        {
+            public AugMix(
+                int severity = 3,
+                int mixture_width = 3,
+                int chain_depth = -1,
+                double alpha = 1.0,
+                bool all_ops = true,
+                InterpolationMode interpolation = InterpolationMode.Bilinear,
+                IList<float>? fill = null)
+            {
+                if (severity < 1 || severity > ParameterMax)
+                    throw new ArgumentException($"The severity must be between [1, {ParameterMax}]. Got {severity} instead.");
+
+                this.severity = severity;
+                this.mixture_width = mixture_width;
+                this.chain_depth = chain_depth;
+                this.alpha = alpha;
+                this.all_ops = all_ops;
+                this.interpolation = interpolation;
+                this.fill = fill;
+            }
+
+            private Dictionary<opType, (Tensor, bool)> augmentation_space(int num_bins, (long height, long width) image_size)
+            {
+                var s = new Dictionary<opType, (Tensor, bool)> {
+                    { opType.ShearX, (torch.linspace(0.0, 0.3, num_bins), true) },
+                    { opType.ShearY, (torch.linspace(0.0, 0.3, num_bins), true) },
+                    { opType.TranslateX, (torch.linspace(0.0, image_size.width / 3.0, num_bins), true) },
+                    { opType.TranslateY, (torch.linspace(0.0, image_size.height / 3.0, num_bins), true) },
+                    { opType.Rotate, (torch.linspace(0.0, 30.0, num_bins), true) },
+                    { opType.Posterize, (4 - (torch.arange(num_bins) / ((num_bins - 1) / 4)).round().@int(), false) },
+                    { opType.Solarize, (torch.linspace(255.0, 0.0, num_bins), false) },
+                    { opType.AutoContrast, (torch.tensor(0.0), false) },
+                    { opType.Equalize, (torch.tensor(0.0), false) }
+                };
+                if (this.all_ops)
+                {
+                    s.Add(opType.Brightness, (torch.linspace(0.0, 0.9, num_bins), true));
+                    s.Add(opType.Color, (torch.linspace(0.0, 0.9, num_bins), true));
+                    s.Add(opType.Contrast, (torch.linspace(0.0, 0.9, num_bins), true));
+                    s.Add(opType.Sharpness, (torch.linspace(0.0, 0.9, num_bins), true));
+                }
+                return s;
+            }
+
+            public Tensor call(Tensor img)
+            {
+                using var _ = torch.NewDisposeScope();
+
+                var (channels, height, width) = F.get_dimensions(img);
+                var op_meta = augmentation_space(ParameterMax, (height, width));
+
+                var orig_dims = img.shape;
+                var batch_view = Enumerable.Repeat(1L, (int)Math.Max(4 - img.ndim, 0)).Concat(orig_dims).ToArray();
+                var batch = img.view(batch_view);
+                var batch_dims = new[]{batch.size(0)}.Concat(Enumerable.Repeat(1L, (int)batch.ndim - 1)).ToArray();
+
+                var m = torch._sample_dirichlet(
+                    torch.tensor(new[]{this.alpha, this.alpha}, device: batch.device).expand(batch_dims[0], -1)
+                    );
+
+                var combined_weights = torch._sample_dirichlet(
+                    torch.tensor(Enumerable.Repeat(this.alpha, this.mixture_width).ToArray(), device: batch.device).expand(batch_dims[0], -1)
+                    ) * m.select(1, 1).view(new[]{batch_dims[0], -1});
+
+                var mix = m.select(1, 0).view(batch_dims) * batch;
+
+                for(int i = 0; i < this.mixture_width; ++i)
+                {
+                    var aug = batch;
+                    var depth = this.chain_depth > 0 ? this.chain_depth : torch.randint(low: 1, high: 4, size: 1).ToInt32();
+                    for(int d = 0; d < depth; ++d)
+                    {
+                        var op_index = torch.randint(op_meta.Count, size: 1).ToInt32();
+                        var op_name = op_meta.Keys.ElementAt(op_index);
+                        var (magnitudes, signed) = op_meta[op_name];
+
+                        var magnitude = magnitudes.ndim > 0 ? magnitudes[torch.randint(this.severity, size: 1).ToInt32()].ToDouble() : 0.0;
+                        if (signed && torch.randint(0, 2, 1).ToBoolean())
+                            magnitude *= -1.0;
+
+                        aug = apply_op(aug, op_name, magnitude, interpolation: this.interpolation, fill: this.fill);
+                    }
+                    mix.add_(combined_weights.select(1, i).view(batch_dims) * aug);
+                }
+
+                mix = mix.view(orig_dims).to(img.dtype);
+
+                return mix.MoveToOuterDisposeScope();
+            }
+
+            private const int ParameterMax = 10;
+            private readonly int severity;
+            private readonly int mixture_width;
+            private readonly int chain_depth;
+            private readonly double alpha;
+            private readonly bool all_ops;
+            private readonly InterpolationMode interpolation;
+            private readonly IList<float>? fill;
+        }
+
         public static partial class transforms
         {
             /// <summary>
@@ -191,6 +291,18 @@ namespace TorchSharp
                 IList<float>? fill = null)
             {
                 return new RandAugment(num_ops, magnitude, num_magnitude_bins, interpolation, fill);
+            }
+
+            static public ITransform AugMix(
+                int severity = 3,
+                int mixture_width = 3,
+                int chain_depth = -1,
+                double alpha = 1.0,
+                bool all_ops = true,
+                InterpolationMode interpolation = InterpolationMode.Bilinear,
+                IList<float>? fill = null)
+            {
+                return new AugMix(severity, mixture_width, chain_depth, alpha, all_ops, interpolation, fill);
             }
         }
     }
