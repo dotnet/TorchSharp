2d8953002b56e63ff9967421ab9cac63019e0559
Shaltiel Shmidman <shaltieltzion@gmail.com>
2023-11-01 18:40:13 +0200

    Added docs

--- src/TorchSharp/Utils/ComponentNameAttribute.cs
+++ src/TorchSharp/Utils/ComponentNameAttribute.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TorchSharp.Utils
 {
+    /// <summary>
+    /// Specifies the custom name for a component to be used in the module's state_dict instead of the default field name.
+    /// </summary>
     public class ComponentNameAttribute : Attribute
     {
         public string Name { get; set; }
