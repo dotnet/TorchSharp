dd8a3b46584cae8fd615474c1b743076cf25b0c5
Shaltiel Shmidman <shaltieltzion@gmail.com>
2024-01-16 19:49:06 +0200

    Added initialize cuda always

--- src/TorchSharp/Device.cs
+++ src/TorchSharp/Device.cs
@@ -80,17 +80,17 @@ namespace TorchSharp
         /// <summary>
         /// Convenience declaration of a CUDA device accessible everywhere.
         /// </summary>
-        public static Device CUDA => InitializeDevice(new Device(DeviceType.CUDA, -1));
+        public static readonly Device CUDA = cuda.is_available() ? new Device(DeviceType.CUDA, -1) : null;
 
         /// <summary>
         /// Convenience declaration of a CPU device accessible everywhere.
         /// </summary>
-        public static Device CPU = InitializeDevice(new Device(DeviceType.CPU, -1));
+        public static readonly Device CPU = InitializeDevice(new Device(DeviceType.CPU, -1));
 
         /// <summary>
         /// Convenience declaration of a META device accessible everywhere.
         /// </summary>
-        public static Device META = InitializeDevice(new Device(DeviceType.META, -1));
+        public static readonly Device META = InitializeDevice(new Device(DeviceType.META, -1));
 
         /// <summary>
         /// Factory for a device object, following the Pytorch API.
