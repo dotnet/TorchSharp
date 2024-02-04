64e3d16690be2056874c9bc1a4dd03f58fca2dcc
Shaltiel Shmidman <shaltieltzion@gmail.com>
2024-02-04 02:52:34 +0200

    Added handling for no grad disposing

--- src/Native/LibTorchSharp/THSAutograd.cpp
+++ src/Native/LibTorchSharp/THSAutograd.cpp
@@ -163,7 +163,7 @@ void THSAutograd_Function_wrapOutputs(TensorArray vars_, TensorArray nonDiff_, T
                 "Please open a feature request on GitHub if you need this.");
         };
 
-    auto res = torch::autograd::_wrap_outputs(vars, nonDiff, dirty, outputs, node.weak_ptr->expired() ? nullptr : node.weak_ptr->lock(), jvp_fn, {});
+    auto res = torch::autograd::_wrap_outputs(vars, nonDiff, dirty, outputs, node.weak_ptr == nullptr || node.weak_ptr->expired() ? nullptr : node.weak_ptr->lock(), jvp_fn, {});
     auto sz = res.size();
 
     Tensor* result = allocator(sz);
