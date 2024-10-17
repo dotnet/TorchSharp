b54552073142e0684d2da363c0f9bd6c4514dff1
Michael Phelps <mvphelps@users.noreply.github.com>
2024-10-17 16:41:40 -0500

    Enable DisposeScope for PackedSequence

--- src/TorchSharp/NN/Utils/PackedSequence.cs
+++ src/TorchSharp/NN/Utils/PackedSequence.cs
@@ -1,4 +1,5 @@
 // Copyright (c) .NET Foundation and Contributors.  All Rights Reserved.  See LICENSE in the project root for license information.
+
 using System;
 using System.Runtime.InteropServices;
 using static TorchSharp.PInvoke.NativeMethods;
@@ -18,6 +19,8 @@ namespace TorchSharp
                     /// </summary>
                     public sealed class PackedSequence : IDisposable
                     {
+                        internal DisposeScope OwningDisposeScope { get; set; }
+
                         /// <summary>
                         /// Class wrapping PyTorch's packedsequence object reference.
                         /// </summary>
@@ -39,6 +42,7 @@ namespace TorchSharp
                             protected override bool ReleaseHandle()
                             {
                                 THSNN_PackedSequence_dispose(handle);
+                                handle = IntPtr.Zero;
                                 return true;
                             }
                         }
@@ -62,6 +66,10 @@ namespace TorchSharp
                         /// The original indices
                         /// </summary>
                         public readonly Tensor unsorted_indices;
+                        /// <summary>
+                        /// Is true if the PackedSequence has been disposed, false otherwise.
+                        /// </summary>
+                        public bool IsInvalid => handle.IsInvalid;
                         private HType handle;
 
                         internal PackedSequence(HType handle)
@@ -71,6 +79,11 @@ namespace TorchSharp
                             this.batch_sizes = new Tensor(THSNN_PackedSequence_batch_sizes(handle));
                             this.sorted_indices = new Tensor(THSNN_PackedSequence_sorted_indices(handle));
                             this.unsorted_indices = new Tensor(THSNN_PackedSequence_unsorted_indices(handle));
+                            OwningDisposeScope = DisposeScopeManager.ThreadSingleton.RegisterOnCurrentDisposeScope(this.data);
+                            OwningDisposeScope = DisposeScopeManager.ThreadSingleton.RegisterOnCurrentDisposeScope(this.batch_sizes);
+                            OwningDisposeScope = DisposeScopeManager.ThreadSingleton.RegisterOnCurrentDisposeScope(this.sorted_indices);
+                            OwningDisposeScope = DisposeScopeManager.ThreadSingleton.RegisterOnCurrentDisposeScope(this.unsorted_indices);
+                            OwningDisposeScope = DisposeScopeManager.ThreadSingleton.RegisterOnCurrentDisposeScope(this);
                         }
 
                         internal HType Handle => handle;
@@ -84,13 +97,67 @@ namespace TorchSharp
                             this.batch_sizes.Dispose();
                             this.sorted_indices.Dispose();
                             this.unsorted_indices.Dispose();
+                            OwningDisposeScope?.MarkAsDisposed(this);
 
                             if (handle != null && !handle.IsInvalid) {
                                 handle.Dispose();
                                 handle.SetHandleAsInvalid();
+
                             }
                         }
-}
+                        /// <summary>
+                        /// Moves PackedSequence to the outer DisposeScope. If there is no outer DisposeScope, it's detached from the
+                        /// DisposeScope system.
+                        /// </summary>
+                        /// <returns>The same PackedSequence that the method was called on</returns>
+                        public PackedSequence MoveToOuterDisposeScope()
+                        {
+                            OwningDisposeScope?.MoveToOuter(this.data);
+                            OwningDisposeScope?.MoveToOuter(this.batch_sizes);
+                            OwningDisposeScope?.MoveToOuter(this.sorted_indices);
+                            OwningDisposeScope?.MoveToOuter(this.unsorted_indices);
+                            OwningDisposeScope?.MoveToOuter(this);
+                            return this;
+                        }
+
+                        /// <summary>
+                        /// Detaches the PackedSequence completely from the DisposeScope system.
+                        /// </summary>
+                        /// <returns>The same PackedSequence that the method was called on</returns>
+                        public PackedSequence DetachFromDisposeScope()
+                        {
+                            OwningDisposeScope?.Detach(this.data);
+                            OwningDisposeScope?.Detach(this.batch_sizes);
+                            OwningDisposeScope?.Detach(this.sorted_indices);
+                            OwningDisposeScope?.Detach(this.unsorted_indices);
+                            OwningDisposeScope?.Detach(this);
+                            return this;
+                        }
+
+                        public PackedSequence MoveToOtherDisposeScope(PackedSequence other)
+                        {
+                            return MoveToOtherDisposeScope(other.OwningDisposeScope);
+                        }
+
+                        public PackedSequence MoveToOtherDisposeScope(DisposeScope other)
+                        {
+                            if (OwningDisposeScope == null && other != null) {
+                                other.Attach(this.data);
+                                other.Attach(this.batch_sizes);
+                                other.Attach(this.sorted_indices);
+                                other.Attach(this.unsorted_indices);
+                                other.Attach(this);
+                            }
+                            else {
+                                OwningDisposeScope?.MoveToOther(other, this.data);
+                                OwningDisposeScope?.MoveToOther(other, this.batch_sizes);
+                                OwningDisposeScope?.MoveToOther(other, this.sorted_indices);
+                                OwningDisposeScope?.MoveToOther(other, this.unsorted_indices);
+                                OwningDisposeScope?.MoveToOther(other, this);
+                            }
+                            return this;
+                        }
+                    }
                 }
             }
         }
