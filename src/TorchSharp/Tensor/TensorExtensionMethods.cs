4c10b0632e1d7bf3d4f31849b79a13e17117ac99
Stefan-Alin Pahontu <56953855+alinpahontu2912@users.noreply.github.com>
2024-11-28 15:13:48 +0100

    test

--- src/TorchSharp/Tensor/TensorExtensionMethods.cs
+++ src/TorchSharp/Tensor/TensorExtensionMethods.cs
@@ -259,7 +259,7 @@ namespace TorchSharp
         /// The style to use -- either 'default,' 'metadata,' 'julia,' or 'numpy'
         /// </param>
         /// <returns></returns>
-        public static Tensor print(this Tensor t, string? fltFormat = "g5", int? width = 100, string? newLine = null, CultureInfo? cultureInfo = null, TensorStringStyle style = TensorStringStyle.Default)
+        public static Tensor print(this Tensor t, string? fltFormat = "g5", int? width = 100, string? newLine = Environment.newLine, CultureInfo? cultureInfo = null, TensorStringStyle style = TensorStringStyle.Default)
         {
             Console.Write(t.ToString(style, fltFormat, width, cultureInfo, newLine));
             return t;
