d22c5856cc99838370397cb45032a374539dfd0d
AhmedZero <ahmels@gmail.com>
2024-12-27 15:41:41 +0200

    Remove IDisposable and constructor checks from Normalize

--- src/TorchVision/Normalize.cs
+++ src/TorchVision/Normalize.cs
@@ -7,16 +7,12 @@ namespace TorchSharp
 {
     public static partial class torchvision
     {
-        internal class Normalize : ITransform, IDisposable
+        internal class Normalize : ITransform
         {
             internal Normalize(double[] means, double[] stdevs,bool inplace = false)
             {
                 if (means is null) throw new ArgumentNullException(nameof(means));
                 if (stdevs is null) throw new ArgumentNullException(nameof(stdevs));
-                if (means.Length != stdevs.Length)
-                    throw new ArgumentException($"{nameof(means)} and {nameof(stdevs)} must be the same length in call to Normalize");
-                if (means.Length != 1 && means.Length != 3)
-                    throw new ArgumentException($"Since they correspond to the number of channels in an image, {nameof(means)} and {nameof(stdevs)} must both be either 1 or 3 long");
                 this.means = means;
                 this.stdevs = stdevs;
                 this.inplace = inplace;
@@ -31,27 +27,7 @@ namespace TorchSharp
             private readonly double[] means;
             private readonly double[] stdevs;
             private readonly bool inplace;
-            bool disposedValue;
-
-            protected virtual void Dispose(bool disposing)
-            {
-                if (!disposedValue) {
-                    disposedValue = true;
-                }
-            }
-
-            ~Normalize()
-            {
-                // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-                Dispose(disposing: false);
-            }
-
-            public void Dispose()
-            {
-                // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-                Dispose(disposing: true);
-                GC.SuppressFinalize(this);
-            }
+         
         }
 
         public static partial class transforms
